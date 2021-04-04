380094b5f25818042db8c4330648d07040fd3296
Research Building Branch <534248377@qq.com>
2021-04-04 22:41:33 +0800

    4.4 evening at research building

--- LDDBP-for-FKP/code/Feature_Extration.py
+++ LDDBP-for-FKP/code/Feature_Extration.py
@@ -2,11 +2,12 @@ import cv2 as cv
 import numpy as np
 import math
 from scipy import io as scio
+from scipy import signal
 np.set_printoptions(threshold=np.inf)
 
 
 class FeatureExtraction:
-    def __init__(self):
+    def __init__(self, name=""):
         self.LAMBDA = 9.9932       # 波长
         # self.LAMBDA = 10.0318       # 波长
 
@@ -20,6 +21,7 @@ class FeatureExtraction:
         self.IMG_ROW = 110          # ROI图像的高，即行数
         self.IMG_COL = 220          # ROI图像的宽，即列数
         self.ROW_PLUS_COL = self.IMG_ROW * self.IMG_COL
+        self.IMG_NAME = name
         # print(self.SIGMA)
 
     def generateGaborKernel(self):      # 计算gabor核
@@ -30,19 +32,33 @@ class FeatureExtraction:
                                                    self.PSI, cv.CV_32F))
         return gabor_kernels
 
-    def obtainGaborKernel(self):        # 读取gabor核
+    def getGaborKernel(self):        # 读取gabor核
         file = scio.loadmat(r"../../31_LDDBP/gaborfilter.mat")
-        return file["filters"][0]
+        kernels = [i for i in file["filters"]]
+        return kernels
 
-    def lddbp_coding(self):
-        gabor_kernels = self.obtainGaborKernel()
-        convolutional_result = np.zeros(self.IMG_ROW, self.IMG_COL, )
+    def lddbp_coding(self):         # 对图片进行LDDBP特征编码
+        """
+        conduct the coding process of the image.
+        :return:
+        """
+        img = cv.imread(self.IMG_NAME, cv.IMREAD_GRAYSCALE)
+        gabor_kernels = self.getGaborKernel()
+        # the process of convolution
+        convolutional_result = []
+        code_length = len(gabor_kernels)
+        for i in range(code_length):     # 对图像进行卷积操作
+            convolutional_result.append(signal.convolve2d(img, gabor_kernels[i][0], mode="same"))
+        # expand the result box, the length is 14(12+2), as it is a circle code.
+        convolutional_result.append(convolutional_result[0])        # result[12] equals result[1]
+        convolutional_result.insert(0, convolutional_result[code_length])   # result[0] equals result[11]
+
 
 if __name__ == '__main__':
     test = FeatureExtraction()
     # gabor_kernels = test.generateGaborKernel()
-    gabor_kernels = test.obtainGaborKernel()
-    print(gabor_kernels.shape)
+    gabor_kernels = test.getGaborKernel()
+    print(gabor_kernels)
     print(type(gabor_kernels))
     print(type(gabor_kernels[0]))
     print(gabor_kernels[0])
--- LDDBP-for-FKP/code/test.py
+++ LDDBP-for-FKP/code/test.py
@@ -2,9 +2,36 @@ import cv2 as cv
 import numpy as np
 import math
 from scipy import io as scio
-np.set_printoptions(threshold=np.inf)
+from scipy import signal
+import matplotlib.pyplot as plt
+# np.set_printoptions(threshold=np.inf)
 
 data = scio.loadmat(r"C:\Users\HPuser\Desktop\Finger-Knuckle-Print-recognition-system\31_LDDBP\gaborfilter.mat")
-print(type(data))
-print(data["filters"][0][0])
-print(data["filters"][0][0].shape)
+# print(data["filters"].shape[0])
+result = [i for i in data["filters"]]
+print(len(result))
+
+#   卷积并显示图像
+# gabor = data["filters"][0][0]
+# img = cv.imread(r"../img/sample.jpg", cv.IMREAD_GRAYSCALE)
+#
+# result1 = signal.convolve2d(img, gabor, mode="same")
+#
+# for i in range(img.shape[0]):
+#     for j in range(img.shape[1]):
+#         img[i, j] = 255 - img[i, j]
+# # cv.imshow("a", img)
+# # cv.waitKey(0)
+#
+# result2 = signal.convolve2d(img, gabor, mode="same")
+#
+# fig, (ax_orig, ax_mag) = plt.subplots(2, 1, figsize=(6, 15))
+# # fig, (ax_mag) = plt.subplots(1, 1)
+# ax_orig.imshow(np.absolute(result1), cmap='gray')
+# ax_orig.set_title('Original')
+# ax_orig.set_axis_off()
+# ax_mag.imshow(np.absolute(result2), cmap='gray')
+# ax_mag.set_title('Gradient magnitude')
+# ax_mag.set_axis_off()
+# # plt.savefig(r"../img/convelutional_result.jpg")
+# plt.show()
